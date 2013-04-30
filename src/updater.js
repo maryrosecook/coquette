d69a440c6c038ae3455703a7ef678a600edfcd7e
Mary Rose Cook <maryrosecook@maryrosecook.com>
2013-04-30 12:02:26 -0400

    Make requestAnimationFrame work in all browsers and add fallback.

--- src/updater.js
+++ src/updater.js
@@ -2,14 +2,12 @@
   var interval = 16;
 
   function Updater() {
+    setupRequestAnimationFrame();
     this.updatees = [];
     this.tick = interval;
     this.prev = new Date().getTime();
 
     var self = this;
-    var requestAnimationFrame = window.requestAnimationFrame
-        || window.mozRequestAnimationFrame
-        || window.webkitRequestAnimationFrame;
     var update = function() {
       var now = new Date().getTime();
       self.tick = now - self.prev;
@@ -37,6 +35,35 @@
           break;
         }
       }
+    }
+  };
+
+  // From: https://gist.github.com/paulirish/1579671
+  // Thanks Erik Möller, Paul and Tino
+  var setupRequestAnimationFrame = function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+      window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+      window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
+        || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame) {
+      window.requestAnimationFrame = function(callback, element) {
+        var currTime = new Date().getTime();
+        var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+        var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+                                   timeToCall);
+        lastTime = currTime + timeToCall;
+        return id;
+      };
+    }
+
+    if (!window.cancelAnimationFrame) {
+      window.cancelAnimationFrame = function(id) {
+        clearTimeout(id);
+      };
     }
   };
 
