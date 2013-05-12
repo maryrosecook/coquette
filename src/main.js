076095c4e4adcd2681235c23908b496c2337286d
Mary Rose Cook <maryrosecook@maryrosecook.com>
2013-05-11 23:11:23 -0400

    Move coquette.js to main.js so we can make dev version of whole lib called coquette.js

---
+++ src/main.js
@@ -0,0 +1,24 @@
+;(function(exports) {
+  var Coquette = function(game, canvasId, width, height, backgroundColor) {
+    coquette = this;
+    this.renderer = new Coquette.Renderer(canvasId, width, height, backgroundColor);
+    this.inputter = new Coquette.Inputter();
+    this.updater = new Coquette.Updater();
+    this.entities = new Coquette.Entities();
+    this.runner = new Coquette.Runner();
+    this.collider = new Coquette.Collider();
+
+    this.updater.add(this.collider);
+    this.updater.add(this.runner);
+    this.updater.add(this.renderer);
+    this.updater.add(game);
+    this.game = game;
+  };
+
+  var coquette;
+  Coquette.get = function() {
+    return coquette;
+  };
+
+  exports.Coquette = Coquette;
+})(this);
