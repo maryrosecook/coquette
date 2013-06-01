04d08e36b9cd0dfd86193212bc0e4cf308517eaf
Mike Hodnick <mike.hodnick@gmail.com>
2013-05-31 23:35:27 -0500

    added support for more keys

--- coquette.js
+++ coquette.js
@@ -253,6 +253,7 @@
       autoFocus = true;
     }
 
+    var self = this;
     var inputReceiverElement = window;
     if (!autoFocus) {
       inputReceiverElement = canvas;
@@ -261,7 +262,7 @@
       // suppress scrolling
       window.addEventListener("keydown", function(e) {
         // space and arrow keys
-        if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        if(self.supressedKeys.indexOf(e.keyCode) > -1) {
           e.preventDefault();
         }
       }, false);
@@ -274,6 +275,7 @@
   Inputter.prototype = {
     _state: {},
     bindings: {},
+    supressedKeys: [32, 37, 38, 39, 40],
 
     state: function(keyCode, state) {
       if (state !== undefined) {
@@ -291,11 +293,88 @@
       this.state(e.keyCode, false);
     },
 
+    BACKSPACE: 8,
+    TAB: 9,
+    ENTER: 13,
+    SHIFT: 16,
+    CTRL: 17,
+    ALT: 18,
+    PAUSE: 19,
+    CAPS_LOCK: 20,
+    ESC: 27,
+    SPACE: 32,
+    PAGE_UP: 33,
+    PAGE_DOWN: 34,
+    END: 35,
+    HOME: 36,
     LEFT_ARROW: 37,
-    RIGHT_ARROW: 39,
     UP_ARROW: 38,
+    RIGHT_ARROW: 39,
     DOWN_ARROW: 40,
-    SPACE: 32
+    INSERT: 45,
+    DELETE: 46,
+    ZERO: 48,
+    ONE: 49,
+    TWO: 50,
+    THREE: 51,
+    FOUR: 52,
+    FIVE: 53,
+    SIX: 54,
+    SEVEN: 55,
+    EIGHT: 56,
+    NINE: 57,
+    A: 65,
+    B: 66,
+    C: 67,
+    D: 68,
+    E: 69,
+    F: 70,
+    G: 71,
+    H: 72,
+    I: 73,
+    J: 74,
+    K: 75,
+    L: 76,
+    M: 77,
+    N: 78,
+    O: 79,
+    P: 80,
+    Q: 81,
+    R: 82,
+    S: 83,
+    T: 84,
+    U: 85,
+    V: 86,
+    W: 87,
+    X: 88,
+    Y: 89,
+    Z: 90,
+    F1: 112,
+    F2: 113,
+    F3: 114,
+    F4: 115,
+    F5: 116,
+    F6: 117,
+    F7: 118,
+    F8: 119,
+    F9: 120,
+    F10: 121,
+    F11: 122,
+    F12: 123,
+    NUM_LOCK: 144,
+    SCROLL_LOCK: 145,
+    SEMI_COLON: 186,
+    EQUAL: 187,
+    COMMA: 188,
+    DASH: 189,
+    PERIOD: 190,
+    FORWARD_SLASH: 191,
+    GRAVE_ACCENT: 192,
+    OPEN_BRACKET: 219,
+    BACK_SLASH: 220,
+    CLOSE_BRACKET: 221,
+    SINGLE_QUOTE: 222
+
   };
   exports.Inputter = Inputter;
 })(typeof exports === 'undefined' ? this.Coquette : exports);
