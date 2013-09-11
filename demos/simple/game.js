c0f9682dd8512ee1e0d0842ae6b0e8c6fde0c210
Mary Rose Cook <maryrosecook@maryrosecook.com>
2013-09-10 21:03:10 -0400

    Update simple demo for latest Coquette.

--- demos/simple/game.js
+++ demos/simple/game.js
@@ -2,10 +2,10 @@
   var Game = function(canvasId, width, height, autoFocus) {
     var coq = new Coquette(this, canvasId, width, height, "#000", autoFocus);
 
-    coq.entities.create(Person, { pos:{ x:243, y:40 }, color:"#099" }); // paramour
-    coq.entities.create(Person, { pos:{ x:249, y:110 }, color:"#f07", // player
+    coq.entities.create(Person, { pos:{ x:250, y:40 }, color:"#099" }); // paramour
+    coq.entities.create(Person, { pos:{ x:256, y:110 }, color:"#f07", // player
       update: function() {
-        if (coq.inputter.state(coq.inputter.UP_ARROW)) {
+        if (coq.inputter.down(coq.inputter.UP_ARROW)) {
           this.pos.y -= 0.4;
         }
       },
