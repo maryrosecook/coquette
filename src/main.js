6a28b91657ec5957a244a0925a8f4830c3aeb454
Eric Bréchemier <eric@bezen.org>
2013-10-15 09:56:46 +0200

    Bug fix: access constructors from shared space in main.js
    The constructors are no longer set on the global this.Coquette
    in the browser, but directly set to 'this' in the shared space.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,22 @@
 this.Coquette = within( "coquette.maryrosecook.com", function() {
+  var
+    Renderer = this.Renderer,
+    Inputter = this.Inputter,
+    Entities = this.Entities,
+    Runner = this.Runner,
+    Collider = this.Collider,
+    Ticker = this.Ticker;
+
   var Coquette = function(game, canvasId, width, height, backgroundColor, autoFocus) {
     var canvas = document.getElementById(canvasId);
-    this.renderer = new Coquette.Renderer(this, game, canvas, width, height, backgroundColor);
-    this.inputter = new Coquette.Inputter(this, canvas, autoFocus);
-    this.entities = new Coquette.Entities(this, game);
-    this.runner = new Coquette.Runner(this);
-    this.collider = new Coquette.Collider(this);
+    this.renderer = new Renderer(this, game, canvas, width, height, backgroundColor);
+    this.inputter = new Inputter(this, canvas, autoFocus);
+    this.entities = new Entities(this, game);
+    this.runner = new Runner(this);
+    this.collider = new Collider(this);
 
     var self = this;
-    this.ticker = new Coquette.Ticker(this, function(interval) {
+    this.ticker = new Ticker(this, function(interval) {
       self.collider.update(interval);
       self.runner.update(interval);
       if (game.update !== undefined) {
