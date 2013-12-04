1195a87b73c23c52a006f5e4c97f6079526d750f
Mary Rose Cook <maryrosecook@maryrosecook.com>
2013-12-03 23:43:24 -0500

    Set good example: store c simple game instance

--- demos/simple/game.js
+++ demos/simple/game.js
@@ -1,24 +1,30 @@
 ;(function(exports) {
   var SimpleGame = function(autoFocus) {
-    var c = new Coquette(this, "simple-canvas", 500, 150, "#000", autoFocus);
+    this.c = new Coquette(this, "simple-canvas", 500, 150, "#000", autoFocus);
 
-    c.entities.create(Person, { center: { x:250, y:40 }, color:"#099" }); // paramour
-    c.entities.create(Person, { center: { x:256, y:110 }, color:"#f07", // player
+    // paramour
+    this.c.entities.create(Person, { center: { x:250, y:40 }, color:"#099" });
+
+    // player
+    this.c.entities.create(Person, { center: { x:256, y:110 }, color:"#f07",
       update: function() {
-        if (c.inputter.isDown(c.inputter.UP_ARROW)) {
+        if (this.c.inputter.isDown(this.c.inputter.UP_ARROW)) {
           this.center.y -= 0.4;
         }
       },
+
       collision: function(other) {
         other.center.y = this.center.y; // follow the player
       }
     });
   };
 
-  var Person = function(_, settings) {
+  var Person = function(game, settings) {
+    this.c = game.c;
     for (var i in settings) {
       this[i] = settings[i];
     }
+
     this.size = { x:9, y:9 };
     this.draw = function(ctx) {
       ctx.fillStyle = settings.color;
